b5d69437b767622a3ba715bf4d21c4b12204c0f6
seemabibi <seema.bibi@hotmail.co.uk>
2022-10-14 16:46:43 +0100

    updated js code

--- scipt.js
+++ scipt.js
@@ -8,4 +8,28 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+   var options = {
+   numberOfChars: 0,
+   hasSpecialChars: false,
+   hasLowerchars: false,
+   hasUpperChars: false,
+   hasNumberChars: false,
+   }
+
+   var length = window.prompt("how long would you like your password to be?");
+
+   if (isNaN (length)) {
+   window.alert ("You must enter a valid number")
+   }
+
+
+
+
+
+
+
+
+
+}
 generateBtn.addEventListener("click", writePassword);
