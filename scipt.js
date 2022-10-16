96a1203d08fe20219a0a5930bbda0520784e4372
seemabibi <seema.bibi@hotmail.co.uk>
2022-10-16 16:19:41 +0100

    added questions for password choice

--- scipt.js
+++ scipt.js
@@ -1,5 +1,7 @@
+// Asignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -17,18 +19,43 @@ function generatePassword() {
    hasNumberChars: false,
    }
   
-   var length = window.prompt("how long would you like your password to be?");
+   // Validate the length that the user has entered 
+   var length = window.prompt(
+    "How many characters would you like your password to contain?"
+    );
 
+    // If the answer is not a number the following alert will appear 
    if (isNaN (length)) {
-   window.alert ("You must enter a valid number")
-   }
-
+   window.alert (
+    "You must enter a valid number");
+    return;
+   } else if (length <8 || length > 128)
+   window.alert(
+    "You must enter a password between 8 to 128 characters"
+   );
+   return;
+  }
    options.numberOfChars = length;
+   // Set questions for the user to specify these password choices
+   options.hasLowerchars = confirm(
+    "Click OK to confirm including lowercase characters"
+   );
+   options.hasUpperChars = window.confirm(
+    "Click OK to confirm including uppercase characters"
+   );
+   options.hasSpecialChars = window.confirm(
+    "Click OK to confirm including special characters"
+   );
+   options.numberOfChars = window.confirm(
+    "Click OK to confirm including numeric characters"
+   );
 
-   options.hasLowerchars = confirm("would you like lower case characters");
+   Const Charlower = [];
+
+
+
+
    
-   var chars = [];
-
    if (options.hasLowerchars) {
 
    }
